90da90dc914076f5028360bcf7130045ab02f9a6
Rajashekarmanda <rajashekarmanda3729@gmail.com>
2023-03-20 14:19:20 +0530

    updated lines

--- findAllUsersStayingInGermany.cjs
+++ findAllUsersStayingInGermany.cjs
@@ -1,7 +1,3 @@
 const users = require('./1-users.cjs')
 
-let germanyUsersArr = []
-for (let [key, val] of Object.entries(users)) {
-    (val.nationality === 'Germany') ? germanyUsersArr.push(key) : '' //(do nothing if false)
-}
-console.log(germanyUsersArr)
+console.log(Object.entries(users).filter(([key,val]) => val.nationality === 'Germany'))
--- findAllUsersWhoAreInterestedInPlayingVideoGames.cjs
+++ findAllUsersWhoAreInterestedInPlayingVideoGames.cjs
@@ -3,7 +3,3 @@ const users = require('./1-users.cjs')
 let filterVideoGameUsers = []
 Object.entries(users).filter((key) => ((key[1].interests.includes('Video Games') || key[1].interests.join(' ').includes('Video Games'))) ? filterVideoGameUsers.push(key) : '')
 console.log(filterVideoGameUsers)
-
-
-
-
